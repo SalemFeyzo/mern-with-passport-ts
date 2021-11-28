a7559f677e3ab45e8f1a02e8c528f465b0e69d49
SalemFeyzo <f1989salim@gmail.com>
2021-11-28 14:10:44 +0300

    fix

--- src/config/connectDB.ts
+++ src/config/connectDB.ts
@@ -2,10 +2,7 @@ import mongoose from "mongoose"
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    })
+    const conn = await mongoose.connect(process.env.MONGODB_URI)
     console.log(`mongodb connected: ${conn.connection.host}`)
   } catch (error) {
     console.error(`Error: ${error.message}`)
