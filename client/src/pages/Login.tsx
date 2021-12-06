7ffb85c0d523ef9b80684f38f80c27c1c0f4ef27
SalemFeyzo <f1989salim@gmail.com>
2021-12-06 12:38:31 +0300

    fix

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -4,7 +4,7 @@ import Github from "../assets/github.svg"
 
 const Login = () => {
   const loginWithGoogle = () => {
-    window.open("http://localhost:5000/auth/google")
+    window.open("http://localhost:5000/auth/google", "_self")
   }
 
   return (
